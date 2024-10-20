c987581ba581d25e2f7ba0bf3c2eaded261ba1cf
kiettt <trinhkiet0105@gmail.com>
2024-10-20 14:23:03 +0700

    refactor: update severve scripts

--- serve_model.sh
+++ serve_model.sh
@@ -1,1 +1,7 @@
-python3 -m vllm.entrypoints.openai.api_server --model "neuralmagic/Qwen2-7B-Instruct-FP8" --gpu-memory-utilization 0.5 --use-v2-block-manager --tensor-parallel-size 2 --port 1478 --enforce-eager 
+vllm serve /HDD/models/ai-tutor/gptq/Qwen2.5-14B-Instruct-GPTQ-Int4 \
+    --served-model-name qwen2.5-72b \
+    --gpu-memory-utilization 0.5 --download-dir /HDD/models/ai-tutor \
+    --tensor-parallel-size 2 \
+    --host 0.0.0.0 --port 8963 \
+    --max-model-len 8192 \
+    --enable-auto-tool-choice --tool-call-parser hermes \
